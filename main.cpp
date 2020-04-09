e3d6d664105e006b51e12d5410da68a71357c3c5
xSlendiX <gamingxslendix@gmail.com>
2020-04-08 17:48:38 -0700

    Did stuff that is broken for some fucking reason help

--- main.cpp
+++ main.cpp
@@ -1,10 +1,39 @@
 #include <iostream>
-
-using namespace std;
+#include <windows.h>
+#include <winuser.h>
+#include <wtsapi32.h>
 
 int main()
 {
-    cout << "Hello world!" << endl;
+    std::cout << "BelleShell has started!" << std::endl;
+
+    std::cout << "Getting all processes and removing window decoration..." << std::endl;
+
+    WTS_PROCESS_INFO* pWPIs = NULL;
+    DWORD dwProcCount = 0;
+    if(WTSEnumerateProcesses(WTS_CURRENT_SERVER_HANDLE, NULL, 1, &pWPIs, &dwProcCount))
+    {
+        //Go through all processes retrieved
+        for(DWORD i = 0; i < dwProcCount; i++)
+        {
+            //pWPIs[i].pProcessName = process file name only, no path!
+            //pWPIs[i].ProcessId = process ID
+            //pWPIs[i].SessionId = session ID, if you need to limit it to the logged in user processes
+            //pWPIs[i].pUserSid = user SID that started the process
+
+            if (GetGUIThreadInfo(pWPIs[i].ProcessId, 0)) {
+                // Process has a window. Print process name.
+                std::cout << "Process found! ID: " << pWPIs[i].ProcessId << " Name: " << pWPIs[i].pProcessName;
+            }
+        }
+    }
+
+    //Free memory
+    if(pWPIs)
+    {
+        WTSFreeMemory(pWPIs);
+        pWPIs = NULL;
+    }
 
     return 0;
 }
